0bed13ce3c04f3aafc043245381563d335a575c1
hanfezh <xianfeng.zhu@gmail.com>
2022-12-09 15:52:51 +0800

    More unit tests for bubble sort.

--- sorting/bubble_sort_test.cpp
+++ sorting/bubble_sort_test.cpp
@@ -17,9 +17,21 @@
  */
 #include "bubble_sort.h"
 
+#include <algorithm>
 #include <array>
+#include <cstdlib>
+#include <ctime>
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+
+template <std::size_t N>
+std::array<int, N> GenRandArray() {
+  std::array<int, N> arr;
+  for (int i = 0; i < N; i++) {
+    arr[i] = std::rand();
+  }
+  return arr;
+}
 
 TEST(BubbleSort, Int1) {
   std::array<int, 1> nums{1};
@@ -29,18 +41,37 @@ TEST(BubbleSort, Int1) {
 
 TEST(BubbleSort, Int5) {
   std::array<int, 5> nums = {0, 4, 3, 2, 1};
+  std::array<int, 5> copy = nums;
   BubbleSort(nums);
-  EXPECT_THAT(nums, testing::ElementsAreArray<int>({0, 1, 2, 3, 4}));
+  std::sort(copy.begin(), copy.end());
+  EXPECT_THAT(nums, testing::ElementsAreArray(copy));
 };
 
 TEST(BubbleSort, Int10) {
   std::array<int, 10> nums = {8, 7, 0, 1, 2, 9, 4, 5, 3, 6};
+  std::array<int, 10> copy = nums;
   BubbleSort(nums);
-  EXPECT_THAT(nums, testing::ElementsAreArray<int>({0, 1, 2, 3, 4, 5, 6, 7, 8, 9}));
+  std::sort(copy.begin(), copy.end());
+  EXPECT_THAT(nums, testing::ElementsAreArray(copy));
 }
 
 TEST(BubbleSort, Duplicated) {
   std::array<int, 6> nums = {3, 0, 1, 5, 3, 0};
+  std::array<int, 6> copy = nums;
   BubbleSort(nums);
-  EXPECT_THAT(nums, testing::ElementsAreArray<int>({0, 0, 1, 3, 3, 5}));
+  std::sort(copy.begin(), copy.end());
+  EXPECT_THAT(nums, testing::ElementsAreArray(copy));
+}
+
+class BubbleSortTest : public testing::Test {
+ protected:
+  void SetUp() override { std::srand(std::time(nullptr)); }
+};
+
+TEST_F(BubbleSortTest, Random) {
+  std::array<int, 100> nums = GenRandArray<100>();
+  std::array<int, 100> copy = nums;
+  BubbleSort(nums);
+  std::sort(copy.begin(), copy.end());
+  EXPECT_THAT(nums, testing::ElementsAreArray(copy));
 }
